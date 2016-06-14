e5c23fcb5b0bcdc35319770f9bd535459d85ad29
bencallis1 <bencallis1@gmail.com>
2016-06-13 21:23:17 -0600

    Added imageList component

---
+++ client/components/imageList.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ben on 6/13/16.
+ */
+import React from 'react'
+
+
+const imageList = () => {
+    return(
+        <ul>
+            <li>Image Detail</li>
+        </ul>
+    );
+};
+
+export default imageList;
