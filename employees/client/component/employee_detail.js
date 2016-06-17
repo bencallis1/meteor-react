400f456d06e00e1f01d73df3ba29d288c83fb67b
bencallis1 <bencallis1@gmail.com>
2016-06-17 15:23:11 -0600

    added a employee_detail component and passed props via map

---
+++ employees/client/component/employee_detail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const EmployeeDetail = () => {
+    return (
+        <div>
+            <div className="employee-list">
+                HEy 
+
+            </div>
+        </div>
+    )
+};
+
+
+export default EmployeeDetail
--- employees/client/component/employee_list.js
+++ employees/client/component/employee_list.js
@@ -1,20 +1,28 @@
-/**
- * Created by Ben on 6/17/16.
- */
-
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
+    console.log(props.employees);
     return (
         <div>
-            <div className="employee-list"></div>
+            <div className="employee-list">
+                {props.employees.map(employee => <EmployeeDetail /> )}
+
+            </div>
         </div>
     )
 };
 
 
-// Here we are wrapping our component in a container so our component will update with new data anytime that data changes. 
+// Here we are wrapping our component in a container so our component will update with new data anytime that data changes.
 export default createContainer(() => {
+
+    // set up the subscription  to out publish from the backend
+    Meteor.subscribe('employees');
+
+    // return an object. Whatever we return will be sent to EmployeeList as props
+    return { employees: Employees.find({}).fetch() };
 
 }, EmployeeList);
